c751abfb1847a0a7f2570cc7f2acf82a8c666e48
andy.rothwell <rothwell.andy@gmail.com>
2018-09-11 10:09:58 -0400

    creates webpack branch for developing layerboard

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const env = process.env.NODE_ENV;
+const isDevelopment = env === 'development';
+
+module.exports = {
+  entry: {
+    app: './src/main.js',
+  },
+  resolve: {
+    mainFields: ['module', 'main', 'browser'],
+  },
+  devtool: isDevelopment ? 'inline-source-map' : 'source-map',
+  devServer: {
+    contentBase: './public',
+    host: process.env.WEBPACK_DEV_HOST,
+    // port: process.env.WEBPACK_DEV_PORT
+    port: 8082
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'images/[name].[ext]?[hash]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
+  ],
+  mode: env,
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/,
+          chunks: 'initial',
+          name: 'vendor',
+          enforce: true,
+          priority: 5,
+        },
+        layerboard: {
+          test: /layerboard/,
+          chunks: 'initial',
+          name: 'layerboard',
+          priority: 10,
+        },
+      },
+    },
+  },
+};
