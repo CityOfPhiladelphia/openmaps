10f0f2413e712a393a688cd73347c0f84e912ed6
Andy Rothwell <rothwell.andy@gmail.com>
2020-10-08 13:22:29 -0400

    uses new layerboard base config commit to fix 2020 imagery

--- src/main.js
+++ src/main.js
@@ -24,7 +24,7 @@ const customComps = {
   'about': modalAbout,
 };
 
-var BASE_CONFIG_URL = 'https://cdn.jsdelivr.net/gh/cityofphiladelphia/layerboard-default-base-config@cd706668166dec6412b4203b120accac97e69209/config.js';
+var BASE_CONFIG_URL = 'https://cdn.jsdelivr.net/gh/cityofphiladelphia/layerboard-default-base-config@6ecbb73ae275facb3448d35f9860831b641220b5/config.js';
 var GATEKEEPER_KEY = 'ec8681f792812d7e3ff15e9094bfd4ad';
 var WEBMAP_ID = '1596df70df0349e293ceec46a06ccc50';
 
